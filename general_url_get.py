4546e9b8ffab112eb2ccbd33231b8bb7c5ee8663
liwenzhang <zhanglw1@shanghaitech.edu.cn>
2016-11-29 22:15:01 +0800

    genaral downloader

--- general_url_get.py
+++ general_url_get.py
@@ -1,21 +1,40 @@
 import requests
 import re
-
 '''
 @author Ehaschia
 '''
 
+# def download_file(url):
+#     local_filename = url.split('/')[-1]
+#     # NOTE the stream=True parameter
+#     r = requests.get(url, stream=True)
+#     with open(local_filename, 'wb') as f:
+#         for chunk in r.iter_content(chunk_size=1024):
+#             if chunk:  # filter out keep-alive new chunks
+#                 f.write(chunk)
+#                 f.flush()
+#     return local_filename
+import sys
+import time
+import urllib
 
-def download_file(url):
-    local_filename = url.split('/')[-1]
-    # NOTE the stream=True parameter
-    r = requests.get(url, stream=True)
-    with open(local_filename, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-                f.flush()
-    return local_filename
+
+def reporthook(count, block_size, total_size):
+    global start_time
+    if count == 0:
+        start_time = time.time()
+        return
+    duration = time.time() - start_time
+    progress_size = int(count * block_size)
+    speed = int(progress_size / (1024 * duration))
+    percent = int(count * block_size * 100 / total_size)
+    sys.stdout.write("\r...%d%%, %d MB, %d KB/s, %d seconds passed" %
+                     (percent, progress_size / (1024 * 1024), speed, duration))
+    sys.stdout.flush()
+
+
+def save(url, filename):
+    urllib.urlretrieve(url, filename, reporthook)
 
 
 def get_real_url(url, num):
@@ -33,10 +52,14 @@ def get_real_url(url, num):
     }
     r = requests.get(url[num], headers=headers, proxies=proxy, allow_redirects=False)
     tmp_url = r.headers._store['location'][1]
-    if re.search('file', tmp_url):
-        r = requests.get(url[num], headers=headers, proxies=proxy)
-        real_url = re.findall('kNO.*";', r.content)[0]
-        real_url = real_url[7:-2]
+    if not re.search('http.*rar', tmp_url):
+        if re.search('file', tmp_url):
+            r = requests.get(url[num], headers=headers, proxies=proxy)
+            real_url = re.findall('kNO.*";', r.content)[0]
+            real_url = real_url[7:-2]
+        else:
+            r = requests.get(tmp_url, headers=headers, proxies=proxy, allow_redirects=False)
+            real_url = r.headers._store['location'][1]
     else:
         real_url = r.headers._store['location'][1]
     return real_url
@@ -50,7 +73,8 @@ if __name__ == '__main__':
     while line:
         to_read_url.append(line[:-1])
         line = tmp_file.readline()
-    for i in range(0, 517):
+    for i in range(0, len(to_read_url)):
         real_url = get_real_url(to_read_url, i)
         print(real_url)
-        download_file(real_url)
+        local_filename = real_url.split('/')[-1]
+        save(real_url, local_filename)
